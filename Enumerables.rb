f48e4df2db45ad6bc6520049c40068c8f33da0eb
Yair fernando <yair.facio11@gmail.com>
2019-12-09 13:57:34 -0600

    Finish the my_each_with_index method

--- Enumerables.rb
+++ Enumerables.rb
@@ -3,7 +3,28 @@
 module Enumerable
 
   def my_each
-    puts yield(self)
+    if block_given?
+      (self.size).times do |i|
+        yield(self[i])
+      end
+    else
+      to_enum(:my_each)
+    end 
+  end
+
+  def my_each_with_index
+    if block_given?
+      i = 0;
+      while self.size > i
+        yield(self[i])
+        i += 1
+      end
+    else
+      to_enum(:my_each_with_index)
+    end
   end
 
 end
+
+
+
